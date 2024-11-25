62b546cfbc8bc381773602a3a1067bb4b8116dda
Pranav Anne <pranav.anne21@gmail.com>
2024-11-25 17:32:47 -0500

    Added timer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
 import QUESTIONS from "../questions.js";
 import quizComplete from "../assets/quiz-complete.png";
+import QuestionTimer from './QuestionTimer.jsx';
 
 export default function Quiz() {
 
@@ -9,6 +10,14 @@ export default function Quiz() {
     const activeQuestionIndex = userAnswers.length;
 
     const isQuizComplete = activeQuestionIndex === QUESTIONS.length;
+
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnswer];
+        })
+    }, [])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (isQuizComplete) {
         return <div id="summary">
@@ -20,16 +29,11 @@ export default function Quiz() {
     const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
     shuffledAnswers.sort((a,b) => Math.random() - 0.5);
 
-    function handleSelectAnswer(selectedAnswer) {
-        setUserAnswers((prevUserAnswers) => {
-            return [...prevUserAnswers, selectedAnswer];
-        })
-    }
-
-
     return (
         <div id="quiz">
             <div id="question">
+                {/* whenever key changes react will remount QuestionTimer */}
+                <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer}></QuestionTimer>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer) => {
