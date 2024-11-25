2bbbab47310b333ad1e938a49d0c95085011534f
Pranav Anne <pranav.anne21@gmail.com>
2024-11-25 16:05:07 -0500

    added quiz component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
 
 function App() {
     return (
         <>
             <Header></Header>
-
+            <main>
+                <Quiz></Quiz>
+            </main>
         </>
     );
 }
