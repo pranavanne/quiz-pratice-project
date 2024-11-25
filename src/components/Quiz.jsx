680577b91c37c024e314ce3a802a5505da8f3700
Pranav Anne <pranav.anne21@gmail.com>
2024-11-25 16:15:28 -0500

    added summary screen and answers are now shuffled.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import QUESTIONS from "../questions.js";
+import quizComplete from "../assets/quiz-complete.png";
 
 export default function Quiz() {
 
@@ -7,18 +8,31 @@ export default function Quiz() {
 
     const activeQuestionIndex = userAnswers.length;
 
+    const isQuizComplete = activeQuestionIndex === QUESTIONS.length;
+
+    if (isQuizComplete) {
+        return <div id="summary">
+            <img src={quizComplete} alt="Trophy icon." />
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort((a,b) => Math.random() - 0.5);
+
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         })
     }
 
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+                    {shuffledAnswers.map((answer) => {
                         return (
                             <li key={answer} className='answer'>
                                 <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
